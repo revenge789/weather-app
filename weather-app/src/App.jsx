f32da7e7fb68a5b4db82cbb3ec92ea6ddfb12f6e
revenge789 <revenge789official@gmail.com>
2024-02-17 17:55:23 -0600

    stuck at using meteo.api but good for now

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,34 +1,52 @@
 import { useEffect, useState } from "react"
 import CurrentWeather from "./CurrentWeather"
 import Header from "./Header"
-import LocationAndTimeButton from "./LocationAndTimeButton"
 import SevenDayForecast from "./SevenDayForecast"
+import { fetchWeatherApi } from "openmeteo"
 
-
+const URL = "https://api.open-meteo.com/v1/gfs";
 function App() {
   const [location, setLocation] = useState({
     latitude: 52.34,
     longitude: 23.53,
   })
+
+  const params = {
+    "latitude": 52.52,
+	  "longitude": 13.41,
+	  "current": ["temperature_2m", "precipitation"],
+	  "daily": ["temperature_2m_max", "temperature_2m_min"],
+	  "timezone": "GMT"
+  }
+
   const [date, setDate] = useState(new Date());
   const [currentWeather, setCurrentWeather] = useState({
     temperature: 67,
     precipitation: 36
   })
+
   const [forecast, setForecast] = useState(
-[ {
-    date: '01/12/12',
-    max: 34,
-    min: 12,
-    temperature: 32
-  },
-  {
-    date: '01/13/12',
-    max: 3321,
-    min: 2,
-    temperature: 934
-  }
-])
+  [ 
+    {
+      date: '01/12/12',
+      max: 34,
+      min: 12,
+      temperature: 32
+    },
+    {
+      date: '01/13/12',
+      max: 3321,
+      min: 2,
+      temperature: 934
+    }
+  ])
+
+  useEffect(() => {
+    fetchWeatherApi(URL, params)
+    .then(responses => {return responses[0]})
+    .then((response)=>{})
+  },[date])
+
   useEffect(() => {
     const id = setInterval(()=>{
       const date = new Date()
@@ -38,18 +56,16 @@ function App() {
     return ()=>{clearInterval(id)}
   }, [])
 
-  async function uploadUserData(e)
+  async function uploadLocation(e)
   {
     console.log('uploading...')
     navigator.geolocation.getCurrentPosition(
       (success) => {
-      setUserData(prevData => {
-        return {
-          ...prevData,
+      setLocation({
           latitude: success.coords.latitude,
           longitude: success.coords.longitude
         }
-        })}, 
+        )}, 
       ((err) => {console.log(err)}));
 
   }
@@ -65,7 +81,7 @@ function App() {
           <SevenDayForecast forecast={forecast}/>
         </div>
         <div className="mt-5">
-          <LocationAndTimeButton uploadUserData={uploadUserData}/>
+          <button onClick={uploadLocation} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Set Location</button>
         </div>
       </div>
       
--- weather-app/src/LocationAndTimeButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ({uploadUserData}) {
-  return (
-    <>
-     <button onClick={uploadUserData} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Set Time and Location</button>
-    </>
-  )
-}
